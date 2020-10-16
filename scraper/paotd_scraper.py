c1800067d614e4445874bbb5da86e6508108bcdd
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-10-16 10:43:26 -0600

    starting album detection

--- scraper/paotd_scraper.py
+++ scraper/paotd_scraper.py
@@ -1,15 +1,26 @@
 import wikipedia
 
 
+def is_album(text):
+    album_words = ["album", "lp", "record"]
+    if any(x in text for x in album_words):
+        return True
+    return False
+
+
 if __name__ == "__main__":
-    with open('../artist_list.txt', 'r') as artistFile:
-        for artist in artistFile:
+    with open('../artist_list.txt', 'r') as artist_file:
+        for artist in artist_file:
             if artist.startswith("#"):
                 continue
             artist = artist.strip()
             print(f"{artist} ...")
             discography = wikipedia.page(f"{artist}_discography")
-            print(discography.title)
-            print("done.")
+            for link in discography.links:
+                link_page = wikipedia.page(link)
+                link_content = link_page.content[:200]
+                if is_album(link_content.lower()):
+                    print(link_page.title)
             # print(discography.content)
             # print(discography.links)
+            print("end.")
