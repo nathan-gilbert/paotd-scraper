16cde355e800e4ee2c13d8e289094886bc6fe9c4
Nathan Gilbert <nathan.gilbert@gmail.com>
2022-02-01 21:07:18 -0700

    divide up scraper fiel

--- paotd-scrapper/paotd_scraper.py
+++ paotd-scrapper/paotd_scraper.py
@@ -1,4 +1,3 @@
-import wikipedia
 import datefinder
 
 
@@ -19,23 +18,4 @@ def get_release_date(text):
     return first_match
 
 
-if __name__ == "__main__":
-    with open('../artist_list.txt', 'r') as artist_file:
-        for artist in artist_file:
-            if artist.startswith("#"):
-                continue
-            artist = artist.strip()
-            print(f"{artist} ...")
-            discography = wikipedia.page(f"{artist}_discography")
-            for link in discography.links:
-                try:
-                    link_page = wikipedia.page(link)
-                except wikipedia.exceptions.PageError:
-                    continue
-                link_content = link_page.content[:200]
-                if is_album(link_content.lower()):
-                    date_str = get_release_date(link_page.content)
-                    print(f"{link_page.title} : {date_str}")
-            # print(discography.content)
-            # print(discography.links)
-            print("end.")
+
---
+++ run_scraper.py
@@ -0,0 +1,22 @@
+import wikipedia
+
+if __name__ == "__main__":
+    with open('../artist_list.txt', 'r') as artist_file:
+        for artist in artist_file:
+            if artist.startswith("#"):
+                continue
+            artist = artist.strip()
+            print(f"{artist} ...")
+            discography = wikipedia.page(f"{artist}_discography")
+            for link in discography.links:
+                try:
+                    link_page = wikipedia.page(link)
+                except wikipedia.exceptions.PageError:
+                    continue
+                link_content = link_page.content[:200]
+                if is_album(link_content.lower()):
+                    date_str = get_release_date(link_page.content)
+                    print(f"{link_page.title} : {date_str}")
+            # print(discography.content)
+            # print(discography.links)
+            print("end."):w
