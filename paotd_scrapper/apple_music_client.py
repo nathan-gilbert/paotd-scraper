d59d1e482e37f7eaf6926f299537690982f143a4
Nathan Gilbert <nathan.gilbert@gmail.com>
2022-02-03 20:53:13 -0700

    working apple music requests

--- paotd_scrapper/apple_music_client.py
+++ paotd_scrapper/apple_music_client.py
@@ -1,11 +1,19 @@
 import applemusicpy
+from dotenv import dotenv_values
 
-secret_key = 'x'
-key_id = 'y'
-team_id = 'z'
+config = dotenv_values("../.env")
+secret_key = config["APPLE_MUSIC_KEY"]
+key_id = config["APPLE_MUSIC_KEY_ID"]
+team_id = config["APPLE_MUSIC_TEAM_ID"]
 
-am = applemusicpy.AppleMusic(secret_key, key_id, team_id)
-results = am.search('travis scott', types=['albums'], limit=5)
 
-for item in results['results']['albums']['data']:
-    print(item['attributes']['name'])
+def get_albums():
+    am = applemusicpy.AppleMusic(secret_key, key_id, team_id)
+    results = am.search('pink floyd', types=['albums'])
+
+    for item in results['results']['albums']['data']:
+        print(item['attributes']['name'])
+
+
+if __name__ == "__main__":
+    get_albums()
